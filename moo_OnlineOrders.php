a036e8145dec8c55987db740bc6cc3c725d3db22
elbanyaoui <elbanyaoui@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-03-22 11:08:46 +0000

    Update the version
    git-svn-id: https://plugins.svn.wordpress.org/clover-online-orders/trunk@1376190 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- moo_OnlineOrders.php
+++ moo_OnlineOrders.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Clover Online Orders
  * Plugin URI:        http://www.merchantech.us
  * Description:       Start taking orders from your Wordpress website and have theme sent to your Clover Station
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Merchantech
  * Author URI:        http://www.merchantech.us
  * License:           Clover app
